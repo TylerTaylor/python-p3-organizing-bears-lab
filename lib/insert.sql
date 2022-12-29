db2e4430df4d43b24a63d11f2136ec076b546458
TylerTaylor <vikingtyty@gmail.com>
2022-12-29 11:54:40 -0600

    Done and passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'm', 'brown', 'chill', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'm', 'brown', 'energetic', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'f', 'brown', 'chill', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'm', 'brown', 'cranky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'f', 'brown', 'chill', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'm', 'brown', 'grumpy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'f', 'brown', 'hungry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'm', 'brown', 'chill', true);
